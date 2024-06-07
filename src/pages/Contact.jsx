81b1817e40fe8f04b408bc367679bd99363382d0
Yoursadik-nazrul <sadikull3@gmail.com>
2024-06-07 23:17:27 +0600

    Contact page helmet addeded

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Contact = () => {
     return (
         <section className="py-6 dark:text-gray-900">
+            <Helmet>
+                <title>
+                    Contact Us || NextNest
+                </title>
+            </Helmet>
             <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
                 <div className="py-6 md:py-0 md:px-6">
                     <h1 className="text-4xl font-bold font-jost">Get in touch</h1>
